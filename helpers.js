547278494871f8a8d439419ac1cd27d1ba7d5caa
sarahzdev <sarah.hg.zhang@gmail.com>
2022-12-05 12:50:35 -0800

    try catch attempt to parse typed data

--- helpers.js
+++ helpers.js
@@ -24,30 +24,25 @@ export const getTransactionToSign = txParams => {
 };
 
 export function getSignVersionByMessageFormat(data) {
-  // V1 format: name, type, value
-  // https://github.com/ethereum/EIPs/pull/712/commits/21abe254fe0452d8583d5b132b1d7be87c0439ca#diff-4a2296091e160bda9c4e9b47f34ea91420677e90d0454ededc48e31314d8642bR62
-  if (
-    data.length > 0 &&
-    data[0]['name'] &&
-    data[0]['type'] &&
-    data[0]['value']
-  ) {
-    return SignTypedDataVersion.V1;
-  } else {
-    // Use encodeData to check if message provided is suitable for V3 or V4
-    // https://github.com/MetaMask/eth-sig-util/blob/9f01c9d7922b717ddda3aa894c38fbba623e8bdf/src/sign-typed-data.ts#L193
+  try {
+    // V1 format: name, type, value
+    // V3 & V4 format: types, primaryType, domain, message
+    const { types, domain, primaryType, message } = JSON.parse(data);
     try {
-      const { types, domain, primaryType, message } = JSON.parse(data);
-      // delete types.EIP712Domain;
+      // Use encodeData to check if message provided is suitable for V3
       const encodedData = TypedDataUtils.encodeData(
         primaryType,
         message,
         types,
-        SignTypedDataVersion.V4
+        SignTypedDataVersion.V3
       );
-      return SignTypedDataVersion.V4;
-    } catch (e) {
       return SignTypedDataVersion.V3;
+    } catch (e) {
+      // V4 supports arrays and recursive types
+      // If the data is formatted for V4, encodeData will throw "Arrays are unimplemented in encodeData: use V4 extension"
+      return SignTypedDataVersion.V4;
     }
+  } catch (e) {
+    return SignTypedDataVersion.V1;
   }
 }
